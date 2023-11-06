0ccc54bdd6e22db178a7e1c1bf451e0d37b74450
Dhrubajyoti Mukherjee <dhrubomukh32@gmail.com>
2023-11-06 23:41:27 +0530

    Time: 283 ms (88.24%), Space: 142 MB (96.99%) - LeetHub

---
+++ 1845-seat-reservation-manager/1845-seat-reservation-manager.cpp
@@ -0,0 +1,30 @@
+class SeatManager {
+public:
+    priority_queue<int, vector<int>, greater<int>>pq;
+    int c = 0;
+    SeatManager(int n) {    
+        
+    }
+    
+    int reserve() {
+        if (pq.size() && pq.top() <= c) {
+            int t = pq.top();
+            pq.pop();
+            return t;
+        }
+        c++;
+        return c;
+    }
+    
+    void unreserve(int seatNumber) {
+        pq.push(seatNumber);
+        
+    }
+};
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager* obj = new SeatManager(n);
+ * int param_1 = obj->reserve();
+ * obj->unreserve(seatNumber);
+ */
